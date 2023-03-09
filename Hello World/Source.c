8039a2fe38ea7bb143023306840ab81aac3857bf
NicolasMCompSci <115744859+NicolasMCompSci@users.noreply.github.com>
2023-03-09 08:21:13 -0500

    Add project files.

---
+++ Hello World/Source.c
@@ -0,0 +1,12 @@
+// CSCN71030 - winter 2023
+// Group Project - Tournament Bracket Generator
+// Nicolas Masquieto, Ethan Groen, Louis He
+
+// This is a simple "Hello World" program as per the GitHub setup requirements
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello World\n");
+	return 0;
+}
