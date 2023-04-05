fae5c7eb55503837d9d4cb67e6aef52f9238d73a
NicolasMCompSci <115744859+NicolasMCompSci@users.noreply.github.com>
2023-04-05 17:33:37 -0400

    Created branch, populated GenericInput header

---
+++ Hello World/GenericInput.c
@@ -0,0 +1,10 @@
+// CSCN71030 - winter 2023
+// Group Project - Tournament Bracket Generator
+// Nicolas Masquieto, Ethan Groen, Louis He
+// 
+// Implementation for some generic input functions
+
+// return integer user input
+int getInt();
+// returns a string user input
+char* getString();
---
+++ Hello World/GenericInput.h
@@ -0,0 +1,11 @@
+#pragma once
+// CSCN71030 - winter 2023
+// Group Project - Tournament Bracket Generator
+// Nicolas Masquieto, Ethan Groen, Louis He
+// 
+// Interface for some generic input functions
+
+// return integer user input
+int getInt();
+// returns a string user input
+char* getString();
