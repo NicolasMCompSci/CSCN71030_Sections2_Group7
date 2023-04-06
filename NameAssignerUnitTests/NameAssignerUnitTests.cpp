c321e3fd179def060f617cf5bf03d157932c289d
NicolasMCompSci <115744859+NicolasMCompSci@users.noreply.github.com>
2023-04-06 00:35:56 -0400

    Added empty name assigner and size selector test case files

---
+++ NameAssignerUnitTests/NameAssignerUnitTests.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" {
+#include "nameInput.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NameAssignerUnitTests
+{
+	TEST_CLASS(NameAssignerUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ SizeSelectorUnitTests/SizeSelectorUnitTests.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" {
+#include "numInput.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SizeSelectorUnitTests
+{
+	TEST_CLASS(SizeSelectorUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
