51e031c015f2bcdf7aac5e1514f33cef3c54a53e
NicolasMCompSci <115744859+NicolasMCompSci@users.noreply.github.com>
2023-04-05 21:49:16 -0400

    Created empty FileIO test case file

---
+++ FileIO_UnitTests/FileIO_UnitTests.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" {
+#include "FileIO.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FileIOUnitTests
+{
+	TEST_CLASS(FileIOUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
