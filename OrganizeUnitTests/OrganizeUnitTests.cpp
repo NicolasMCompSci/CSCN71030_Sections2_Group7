80aa4ce75f9948568da84188569cd755d1699c93
NicolasMCompSci <115744859+NicolasMCompSci@users.noreply.github.com>
2023-04-06 00:41:37 -0400

    Added empty test case file for organize component

---
+++ OrganizeUnitTests/OrganizeUnitTests.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" {
+#include "organize.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace OrganizeUnitTests
+{
+	TEST_CLASS(OrganizeUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
