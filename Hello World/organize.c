b04888a74a3b9f49b3e22d4da0a93ed7c0b41e5b
Louis He <Lhe5921@conestogac.on.ca>
2023-04-06 11:10:30 -0400

    Update Organize function and also add Unit Test of it

--- Hello World/organize.c
+++ Hello World/organize.c
@@ -10,15 +10,17 @@
 #include "GenericInput.h"
 
 
+
 void printParticipants(PARTICIPANT* participants, int num)						// Display Tournament
 {
 	printf("List of Participants:\n");
 	printf("-----------------------------------\n");
 	for (int i = 0; i < num; i++) {
-		printf("ID: %d, Name: %s, Group: %d\n", participants[i].id, participants[i].name, participants[i].group_id);
+		printf("ID: %d, Name: %s\n", participants[i].id, participants[i].name);
 	}
 	printf("-----------------------------------\n");
 }
+
 
 
 void OrganizeMenu(PARTICIPANT* participants, int num)										//Tournament Organization Menu
@@ -47,16 +49,7 @@ void OrganizeMenu(PARTICIPANT* participants, int num)										//Tournament Orga
 	printParticipants(participants, num);
 }
 
-void assign(PARTICIPANT* participants, int num)											//Assign to different group
-{
-	for (int i = 0; i < num; i++) {														// Calculate the number on the name
-		int num = 0;
-		for (int j = 0; j < strlen(participants[i].name); j++) {
-			num += participants[i].name[j];
-		}
-		participants[i].group_id = num % 4 + 1;											// Move it to group
-	}
-}	
+
 
 void random(PARTICIPANT * participants, int num)										//Random Function
 {
--- OrganizeUnitTests/OrganizeUnitTests.cpp
+++ OrganizeUnitTests/OrganizeUnitTests.cpp
@@ -3,18 +3,93 @@
 
 extern "C" {
 #include "organize.h"
+#include "GenericInput.h"
+
 }
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace OrganizeUnitTests
 {
-	TEST_CLASS(OrganizeUnitTests)
-	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
+    TEST_CLASS(OrganizeUnitTests)
+    {
+    public:
+		TEST_METHOD(TestRandomFunctionWithDifferentNum)
 		{
+			PARTICIPANT participants[5] = { {0, "Tom"}, {0, "Jerry"}, {0, "Mike"}, {0, "Mary"}, {0, "Lucy"} };
+			int num1 = 5;
+			int num2 = 10;
+
+			random(participants, num1);
+			int sum1 = 0;
+			for (int i = 0; i < num1; i++) 
+			{
+				sum1 += participants[i].id;
+			}
+
+			random(participants, num2);
+			int sum2 = 0;
+			for (int i = 0; i < num2; i++) 
+			{
+				sum2 += participants[i].id;
+			}
+
+			Assert::AreNotEqual(sum1, sum2);
+		}
+
+		TEST_METHOD(TestRandomFunctionWithSameNum)
+		{
+			// Arrange
+			PARTICIPANT participants[3] = { {0, "Tom"}, {0, "Jerry"}, {0, "Mike"} };
+			int num = 3;
+
+			// Act
+			random(participants, num);
+			int sum1 = 0;
+			for (int i = 0; i < num; i++) 
+			{
+				sum1 += participants[i].id;
+			}
+
+			random(participants, num);
+			int sum2 = 0;
+			for (int i = 0; i < num; i++) 
+			{
+				sum2 += participants[i].id;
+			}
+
+			// Assert
+			Assert::AreNotEqual(sum1, sum2);
+		}
+
+		TEST_METHOD(TestMannualValidInput)
+		{
+			PARTICIPANT participants[] = { {1, "Alice", 0}, {2, "Bob", 0}, {3, "Charlie", 0} };
+			const int num = 3;
+
+			mannual(participants, num);
+
+			// Assert that each participant has been assigned a valid ID
+			for (int i = 0; i < num; i++) 
+			{
+				Assert::IsTrue(participants[i].id > 0);
+			}
+		}
+
+		TEST_METHOD(TestMannualInvalidInput)
+		{
+			PARTICIPANT participants[] = { {1, "Alice", 0}, {2, "Bob", 0}, {3, "Charlie", 0} };
+			const int num = 3;
+
+			// Simulate user input of invalid number
+			char input[] = "invalid\n";
+			int ret = scanf_s(input, "%d", &participants[0].id);
+			mannual(participants, num);
+
+			for (int i = 0; i < num; i++) {
+				Assert::IsTrue(participants[i].id > 0);
+			}
 		}
 	};
+
 }
